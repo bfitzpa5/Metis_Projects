e4e4c0a8953d4df7639d8f19e945240f82217b0e
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2018-12-30 20:29:03 -0500

    Make executable and add all functions and exceptions

--- 02-luther/box_office_mojo_scraper.py
+++ 02-luther/box_office_mojo_scraper.py
@@ -1,50 +1,90 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
 import re
+import string
+from collections import defaultdict
+
+WAIT_TIME = 5
 
 """
 Use Big Lebowski as example
+star_wars_url = 'https://www.boxofficemojo.com/movies/?id=starwars8.htm'
+lebow_url = 'http://boxofficemojo.com/movies/?id=biglebowski.htm'
+webbrowser.open_new_tab(lebow_url)
+webbrowser.open_new_tab(star_wars_url)
 """
-mojo_url = 'http://boxofficemojo.com/movies/?id=biglebowski.htm'
-imdb_url = 'https://www.imdb.com/title/tt0118715/'
-webbrowser.open_new_tab(mojo_url)
-webbrowser.open_new_tab(imdb_url)
 
-response = requests.get(mojo_url)
-response.status_code
+def main(argv):
+    url = 'https://www.boxofficemojo.com/movies/?id=starwars8.htm'
+    movie = 'Star Wars: The Last Jedi'
+    field_dict = {'release_date': 'Release Date:', 'distributor': 'Distributor',
+                  'rating': 'MPAA Rating', 'genre': 'Genre: ',
+                  'runtime': 'Runtime:', 'budget': 'Production Budget:'}
+    try:
+        row = movie_scrape(url, field_dict)
+    except HttpStatusCodeError as e:
+        print("HTTP Error %s: %s" (e.code, e.url))
+    print(row)
 
-page = response.text
-soup = BeautifulSoup(page, 'html5lib')
+def movie_scrape(url, field_dict):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise HttpStatusCodeError(url, code)
+    soup = BeautifulSoup(response.text, 'html5lib')
+    row = dict()
+    for col, field_name in field_dict.items():
+        row[col] = movie_val(soup, field_name)
+    row['director'] = table_list(soup, 'Director')
+    row['actors'] = table_list(soup, 'Actors')
+
+    rows = table_rows(soup, re.compile('Domestic.*Summary'))
+    row['open_wkend_gross'] = table_val(rows, 'Opening.*Weekend')
+    row['widest_release'] = (table_val(rows, 'Widest.*Release')
+                             .replace(' theaters', ''))
+    row['in_release'] = table_val(rows, 'In.*Release')
+    return row
 
 def movie_val(soup, field_name):
-    '''Grab a value from boxofficemojo HTML
-    
+    """Grab a value from boxofficemojo HTML
+
     Takes a string attribute of a movie on the page and
     returns the string in the next sibling object
     (the value for that attribute)
     or None if nothing is found.
-    '''
+    """
     obj = soup.find(text=re.compile(field_name))
-    if not obj: 
-        return None
-    # this works for most of the values
-    next_sibling = obj.findNextSibling()
-    if next_sibling:
-        return next_sibling.text 
-    else:
-        return None
+    if obj: 
+        next_sibling = obj.findNextSibling()
+        if next_sibling:
+            return next_sibling.text
+    return None
 
-field_dict = {'release_date': 'Release Date', 'distributor': 'Distributor',
-              'genre': 'Genre', 'rating': 'MPAA Rating',
-              'runtime': 'Runtime', 'budget': 'Production Budget',
-              'director': 'Director', 'actors': 'Actors',
-              'num_theaters': 'Number of Theaters',
-              'opening_wkend_gross': 'Opening Weekend'}
+def table_list(soup, pat):
+    actor_tags =  (soup.find('td', text=re.compile(pat))
+                   .findNextSibling()
+                   .find_all('a'))
+    return ', '.join([x.text for x in actor_tags])
 
-"""
-Test fields
-"""
-for col, field_name in field_dict.items():
-    print('%-20s%-50s' % (col, movie_val(soup, field_name))) 
+def table_rows(soup, pat):
+    return (soup.find('div', text=pat)
+            .findNextSibling()
+            .find_all('tr'))
 
+def table_val(rows, pat):
+    for tr in rows:
+        for td in tr.find_all('td'):
+            if re.compile(pat).search(td.text):
+                t = td.findNextSibling().text
+                return ''.join([x if ord(x) < 128 else ' ' for x in t]).strip()
+    return None
+
+class HttpStatusCodeError(Exception):
+
+    def __init__(self, url, code):
+        self.url = url
+        self.code = code
+
+if __name__ == '__main__':
+    main(sys.argv)
