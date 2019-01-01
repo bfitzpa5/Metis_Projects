3a6d862ed67758aed4ae37e15de7a35c99af2744
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2018-12-31 20:14:13 -0500

    Remove try-except wrapper and HttpStatusCodeError

--- 02-luther/box_office_mojo_scraper.py
+++ 02-luther/box_office_mojo_scraper.py
@@ -25,36 +25,36 @@ def main(argv):
                   'rating': 'MPAA Rating', 'genre': 'Genre: ',
                   'runtime': 'Runtime:', 'budget': 'Production Budget:'}
     records = list()
-    try:
-        url = 'https://www.boxofficemojo.com/yearly/chart/?yr=2018&p=.htm'
-        response = requests.get(url)
-        if response.status_code != 200:
-            raise HttpStatusCodeError(url, code)
-        soup = BeautifulSoup(response.text, 'html5lib')
-        text_pat = re.compile('Movie.*Title.*(click.*to.*view)')
-        row_iter = (soup.find('td', text=text_pat).parent.next_siblings)
-        end_pat = re.compile('Summary.*of.*Movies on Chart')
-        row = next(row_iter)
-        count = 0
-        while row and end_pat.search(row.text) is None:
-            tag = row.find('a')
-            url = tag['href']
-            movie = tag.text
-            count += 1
-            print('Scraping Movie #%d:\t%s...' % (count, movie), end='')
-            records.append(movie_scrape(movie, url, field_dict))
+    url = 'https://www.boxofficemojo.com/yearly/chart/?yr=2018&p=.htm'
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise HttpStatusCodeError(url, code)
+    soup = BeautifulSoup(response.text, 'html5lib')
+    text_pat = re.compile('Movie.*Title.*(click.*to.*view)')
+    row_iter = (soup.find('td', text=text_pat).parent.next_siblings)
+    end_pat = re.compile('Summary.*of.*Movies on Chart')
+    row = next(row_iter)
+    count = 0
+    while row and end_pat.search(row.text) is None:
+        tag = row.find('a')
+        url = tag['href']
+        movie = tag.text
+        count += 1
+        print('Scraping Movie #%d:\t%s...' % (count, movie), end='')
+        record = movie_scrape(movie, url, field_dict)
+        if record:
+            records.append(record)
             print('Complete')
-            time.sleep(WAIT_TIME)
-            row = next(row_iter)
-    except HttpStatusCodeError as e:
-        print("HTTP Error %s: %s" (e.code, e.url))
+        time.sleep(WAIT_TIME)
+        row = next(row_iter)
     
-    pd.DataFrame(records).to_csv('test_18.csv')
+    pd.DataFrame(records).to_csv('data/test_18.csv', index=False)
 
 def movie_scrape(movie, url, field_dict):
     response = requests.get(base_url + url)
     if response.status_code != 200:
-        raise HttpStatusCodeError(url, code)
+        print("HTTP Error %s: %s" (e.code, e.url))
+        return None
     soup = BeautifulSoup(response.text, 'html5lib')
     record = dict()
     record['movie'] = movie
@@ -87,10 +87,11 @@ def movie_val(soup, field_name):
     return None
 
 def table_list(soup, pat):
-    actor_tags =  (soup.find('td', text=re.compile(pat))
-                   .findNextSibling()
-                   .find_all('a'))
-    return ', '.join([x.text for x in actor_tags])
+    td = soup.find('td', text=re.compile(pat))
+    if td:
+        actor_tags =  td.findNextSibling().find_all('a')
+        return ', '.join([x.text for x in actor_tags])
+    return None
 
 def table_rows(soup, pat):
     return (soup.find('div', text=pat)
@@ -104,12 +105,6 @@ def table_val(rows, pat):
                 t = td.findNextSibling().text
                 return ''.join([x if ord(x) < 128 else ' ' for x in t]).strip()
     return None
-
-class HttpStatusCodeError(Exception):
-
-    def __init__(self, url, code):
-        self.url = url
-        self.code = code
 
 if __name__ == '__main__':
     main(sys.argv)
