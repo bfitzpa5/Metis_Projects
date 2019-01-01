d10beaeca9e80de50fc19f1b4df5c33b26e4be92
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2018-12-31 19:30:51 -0500

    Updated to scrape 2018 page

--- 02-luther/box_office_mojo_scraper.py
+++ 02-luther/box_office_mojo_scraper.py
@@ -4,47 +4,72 @@ from bs4 import BeautifulSoup
 import webbrowser
 import re
 import string
+import pandas as pd
 from collections import defaultdict
+import time
 
 WAIT_TIME = 5
+base_url = 'http://www.boxofficemojo.com'
 
 """
 Use Big Lebowski as example
 star_wars_url = 'https://www.boxofficemojo.com/movies/?id=starwars8.htm'
 lebow_url = 'http://boxofficemojo.com/movies/?id=biglebowski.htm'
+yr_url = 'https://www.boxofficemojo.com/yearly/'
 webbrowser.open_new_tab(lebow_url)
 webbrowser.open_new_tab(star_wars_url)
 """
 
 def main(argv):
-    url = 'https://www.boxofficemojo.com/movies/?id=starwars8.htm'
-    movie = 'Star Wars: The Last Jedi'
     field_dict = {'release_date': 'Release Date:', 'distributor': 'Distributor',
                   'rating': 'MPAA Rating', 'genre': 'Genre: ',
                   'runtime': 'Runtime:', 'budget': 'Production Budget:'}
+    records = list()
     try:
-        row = movie_scrape(url, field_dict)
+        url = 'https://www.boxofficemojo.com/yearly/chart/?yr=2018&p=.htm'
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise HttpStatusCodeError(url, code)
+        soup = BeautifulSoup(response.text, 'html5lib')
+        text_pat = re.compile('Movie.*Title.*(click.*to.*view)')
+        row_iter = (soup.find('td', text=text_pat).parent.next_siblings)
+        end_pat = re.compile('Summary.*of.*Movies on Chart')
+        row = next(row_iter)
+        count = 0
+        while row and end_pat.search(row.text) is None:
+            tag = row.find('a')
+            url = tag['href']
+            movie = tag.text
+            count += 1
+            print('Scraping Movie #%d:\t%s...' % (count, movie), end='')
+            records.append(movie_scrape(movie, url, field_dict))
+            print('Complete')
+            time.sleep(WAIT_TIME)
+            row = next(row_iter)
     except HttpStatusCodeError as e:
         print("HTTP Error %s: %s" (e.code, e.url))
-    print(row)
+    
+    pd.DataFrame(records).to_csv('test_18.csv')
 
-def movie_scrape(url, field_dict):
-    response = requests.get(url)
+def movie_scrape(movie, url, field_dict):
+    response = requests.get(base_url + url)
     if response.status_code != 200:
         raise HttpStatusCodeError(url, code)
     soup = BeautifulSoup(response.text, 'html5lib')
-    row = dict()
+    record = dict()
+    record['movie'] = movie
+    record['url'] = url
     for col, field_name in field_dict.items():
-        row[col] = movie_val(soup, field_name)
-    row['director'] = table_list(soup, 'Director')
-    row['actors'] = table_list(soup, 'Actors')
+        record[col] = movie_val(soup, field_name)
+    record['director'] = table_list(soup, 'Director')
+    record['actors'] = table_list(soup, 'Actors')
 
     rows = table_rows(soup, re.compile('Domestic.*Summary'))
-    row['open_wkend_gross'] = table_val(rows, 'Opening.*Weekend')
-    row['widest_release'] = (table_val(rows, 'Widest.*Release')
+    record['open_wkend_gross'] = table_val(rows, 'Opening.*Weekend')
+    record['widest_release'] = (table_val(rows, 'Widest.*Release')
                              .replace(' theaters', ''))
-    row['in_release'] = table_val(rows, 'In.*Release')
-    return row
+    record['in_release'] = table_val(rows, 'In.*Release')
+    return record
 
 def movie_val(soup, field_name):
     """Grab a value from boxofficemojo HTML
