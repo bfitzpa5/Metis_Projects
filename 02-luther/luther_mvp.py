fccadd55ea1e5b73162cfb40d97b9946b03e0ad1
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-01-20 13:05:37 -0500

    Added log_gross and updated lm3

--- 02-luther/luther_mvp.py
+++ 02-luther/luther_mvp.py
@@ -12,18 +12,18 @@ import scipy.stats as stats
 
 fname = sorted([x for x in os.listdir('data')
                 if re.match('box_office_mojo_pp', x)])[-1]
-df = pd.read_csv('data/%s' % fname)
+df = (pd.read_csv('data/%s' % fname)
+      .assign(release_date=lambda x: x.release_date.astype('datetime64'),
+              log_gross=lambda x: np.log(x.domestic_total_gross)))
 
+(df.drop('domestic_total_gross', axis=1)
+ .corr()['log_gross'].abs().sort_values(ascending=False))
 
-df.corr()['domestic_total_gross'].abs().sort_values(ascending=False)
-
-np.log(df.budget).plot.box()
-
-lm1 = smf.ols('domestic_total_gross ~ open_wkend_gross', data=df)
+lm1 = smf.ols('log_gross ~ open_wkend_gross', data=df)
 fit1 = lm1.fit()
 fit1.summary()
 
-lm2 = smf.ols('domestic_total_gross ~ widest_release + runtime', data=df)
+lm2 = smf.ols('domestic_total_gross ~ widest_release', data=df)
 fit2 = lm2.fit()
 fit2.summary()
 
@@ -31,6 +31,14 @@ lm3 = smf.ols('log_gross ~ widest_release + runtime', data=df)
 fit3 = lm3.fit()
 fit3.summary()
 
+plt.gcf().clear()
+fit3.resid.plot(style='o')
+
 lm4 = smf.ols('log_gross ~ widest_release + runtime + widest_release*runtime', data=df)
 fit4 = lm4.fit()
 fit4.summary()
+
+lm3 = smf.ols('log_gross ~ widest_release + runtime', data=df)
+fit3 = lm3.fit()
+fit3.summary()
+
