a3b64cdc065e5379ec13e4ae5a9b74b6ea3c2092
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-03 10:52:27 -0500

    Added ridge regression analysis

--- 02-luther/luther_cross_val.py
+++ 02-luther/luther_cross_val.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score
 import scipy.stats as stats
 %pylab
@@ -24,7 +25,6 @@ df = (pd.read_csv('data/%s' % fname)
                                              .astype('int64')))
       .set_index('title'))
 
-df.columns
 drop_cols = ['domestic_total_gross', 'genre', 'open_wkend_gross', 'rating',
              'log_gross', 'release_date', 'in_release']
 X = df.drop(drop_cols, axis=1)
@@ -41,9 +41,11 @@ reg = LinearRegression()
 scores = cross_val_score(reg, X, y, cv=k_fold, scoring='neg_mean_squared_error',
                          n_jobs=-1)
 print(-scores)
-np.sqrt(-scores).mean() / y.mean()
+-scores.mean()
 
-reg.predict(X.iloc[3])
+reg.fit(X, y)
+
+reg.predict(X.iloc[3, ].reshape(1, -1))
 
 reg = LinearRegression()
 scores = cross_val_score(reg, X, y, cv=10, scoring='r2', n_jobs=-1)
@@ -58,6 +60,10 @@ scores = cross_val_score(reg, X, y, cv=k_fold, scoring='neg_mean_squared_error',
 print(['{:,.0}'.format(np.sqrt(x)) for x in -scores])
 print('{:,.0}'.format(np.sqrt(-scores).mean()))
 
+reg.fit(X, y)
+print(X.columns)
+print(reg.coef_)
+
 y = df.loc[:, 'domestic_total_gross']
 k_fold = KFold(n_splits=10)
 reg = LinearRegression()
@@ -65,3 +71,29 @@ scores = cross_val_score(reg, X, y, cv=k_fold, scoring='r2',
                          n_jobs=-1)
 print(['{:,.2%}'.format(x) for x in scores])
 print('Mean R-Squared: {:,.2%}'.format(scores.mean()))
+
+X = df.drop(drop_cols, axis=1)
+y = df.loc[:, 'log_gross']
+
+print(alphas)
+
+alphas = [0, 1/3**3, 1/3**2, 1/3]
+poly = PolynomialFeatures(degree=2)
+X_ = poly.fit_transform(X)
+X_ = pd.DataFrame(X_, columns=poly.get_feature_names(X.columns))
+print('%-10s%-10s' % ('Alpha', 'R-Squared'))
+for alpha in alphas:
+    reg = Ridge(alpha=alpha)
+    reg.fit(X_, y)
+    print('%-10f%-10f' % (alpha, reg.score(X_, y)))
+
+pd.DataFrame({'coefficients': reg.coef_}, index=X_.columns)
+
+alphas = [0, 1/3**3, 1/3**2, 1/3]
+poly = PolynomialFeatures(degree=3)
+X_ = poly.fit_transform(X)
+print('%-10s%-10s' % ('Alpha', 'Mean R-Squared'))
+for alpha in alphas:
+    reg = Ridge(alpha=alpha)
+    reg.fit(X_, y)
+    print('%-10f%-10f' % (alpha, reg.score(X_, y)))
