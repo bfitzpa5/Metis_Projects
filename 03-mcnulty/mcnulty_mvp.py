0e471e5da1795da26f9e32dad3c23fd0645e9271
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-16 18:56:04 -0500

    Added confusion matrix

--- 03-mcnulty/mcnulty_mvp.py
+++ 03-mcnulty/mcnulty_mvp.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
 %pylab
 
 data_dict = pd.read_excel('data/LCDataDictionary.xlsx')
@@ -53,19 +54,16 @@ X = df.drop('default', axis=1)
 y = df['default']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
+percent_non_default = (df.default.value_counts()
+                       .div(df.shape[0])
+                       .apply('{:.3%}'.format)
+                       .loc[0, ])
+print("Baseline (Percentage of Non-Default) is %s" % percent_non_default)
 logreg = LogisticRegression(C=100000)
 logreg.fit(X_train, y_train)
 accuracy_train = logreg.score(X_train, y_train)
 accuracy_test = logreg.score(X_test, y_test)
 print("train_score=%.3f\ntest_score =%.3f\n" % (accuracy_train*100, accuracy_test*100))
 
-logreg.intercept_
-
-percent_non_default = (df.default.value_counts()
-                       .div(df.shape[0])
-                       .apply('{:.3%}'.format)
-                       .loc[0, ])
-print("Baseline (Percentage of Non-Default) is %s" % percent_non_default)
-
-df.loc[:, 'mths_since_last_delinq'].value_counts()
-
+y_pred = logreg.predict(X_test)
+confusion_matrix(y_test, y_pred)
