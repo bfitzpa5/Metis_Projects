eb6ed16e7fd5636f7641894aa63939bd3b7768f1
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-16 11:56:13 -0500

    Update date formatting in mcnulty_preproc.py

--- 03-mcnulty/mcnulty_preproc.py
+++ 03-mcnulty/mcnulty_preproc.py
@@ -8,17 +8,34 @@ import seaborn as sns
 
 data_dict = pd.read_excel('data/LCDataDictionary.xlsx')
 
-df = pd.read_csv('data/loan.csv')
+"""
+df = (pd.read_csv('data/loan.csv')
+      .assign(issue_d_datetime=lambda x: x.issue_d.astype('datetime64')))
 
-df.shape
+(df.loc[:, ['issue_d', 'issue_d_datetime']]
+ .drop_duplicates()
+ .sort_values('issue_d_datetime')
+ .to_csv(os.path.join('data', 'datetime_test.csv')))
+"""
 
-df.columns
+df = (pd.read_csv('data/loan.csv')
+      .assign(issue_d=lambda x: x.issue_d.astype('datetime64')))
+
+print("Date Range: %s to %s" % (df.issue_d.min().isoformat()[:-9],
+                                df.issue_d.max().isoformat()[:-9]))
 
 df.loan_status.unique()
 
-df.issue_d.unique()
+df.loan_status.value_counts().plot.bar()
 
-print("Date Range: %s to %s" % (df.issue_d.min(), df.issue_d.max()))
+(df.loc[df.loan_status.str.contains('Does not'), ['issue_d']]
+ .drop_duplicates()
+ .sort_values(by='issue_d'))
+
+""" note to BF, figure out way to color bars """
+xticks = df.issue_d.drop_duplicates().sort_values().dt.strftime('%Y-%m').values
+ax = df.issue_d.value_counts().plot.bar()
+ax.set_xticklabels(xticks)
 
 df.loan_status.value_counts()
 
