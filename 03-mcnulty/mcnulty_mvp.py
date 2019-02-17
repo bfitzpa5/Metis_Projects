b1cad654ae6ad89045a97fb901f5c110ba0cc0d2
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-17 10:38:34 -0500

    Updated mvp for mcnulty_preproc file importing

--- 03-mcnulty/mcnulty_mvp.py
+++ 03-mcnulty/mcnulty_mvp.py
@@ -11,45 +11,9 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 data_dict = pd.read_excel('data/LCDataDictionary.xlsx')
 
-def status_filter(input_df):
-    df = input_df.copy()
-    statuses = ['Fully Paid', 'Charged Off', 'Default']
-    return df.loc[df.loan_status.isin(statuses), ]
-
-def remove_annual_inc_nas(input_df):
-    df = input_df.copy()
-    return df.loc[~df.annual_inc.isna(), ]
-
-def emp_length_filter(input_df):
-    df = input_df.copy()
-    na_mask = df.emp_length.isna() | (df.emp_length == 'n/a')
-    return df.loc[~na_mask, ]
-
-def make_dummy(input_df, column):
-    df = input_df.copy()
-    home_dum = (df.loc[:, [column]].pipe(pd.get_dummies))
-    return df.join(home_dum).drop(column, axis=1)
-
-relevant_features = ['issue_d_ord', 'loan_amnt', 'int_rate', 'term',
-                     'grade', 'emp_length', 'home_ownership',
-                     'annual_inc', 'default']
-df = (pd.read_csv('data/loan.csv', low_memory=False)
-      .assign(issue_d=lambda x: x.issue_d.astype('datetime64'))
-      .pipe(status_filter)
-      .pipe(remove_annual_inc_nas)
-      .pipe(emp_length_filter)
-      .query('home_ownership not in ["NONE", "ANY"]')
-      .assign(default=lambda x: np.where(x.loan_status=='Fully Paid', 0, 1),
-              issue_d_ord=lambda x: x.issue_d.apply(dt.datetime.toordinal),
-              term=lambda x: x.term.str.strip())
-      .loc[:, relevant_features]
-      .pipe(make_dummy, 'term')
-      .pipe(make_dummy, 'home_ownership')
-      .pipe(make_dummy, 'emp_length')
-      .pipe(make_dummy, 'grade'))
-
-df.columns
-
+fname = sorted([x for x in os.listdir('data')
+                if re.match('loan_cleaned_', x)])[-1]
+df = pd.read_csv('data/%s' % fname)
 X = df.drop('default', axis=1)
 y = df['default']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
