6bf38b52ccf265f16d9f1c4a3322d8520b6c2dbe
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-22 20:21:20 -0500

    Update features included in mcnulty_preproc.py

--- 03-mcnulty/mcnulty_preproc.py
+++ 03-mcnulty/mcnulty_preproc.py
@@ -5,11 +5,14 @@ import numpy as np
 import datetime as dt
 
 def main(argv):
+
     file_dt = dt.datetime.now().isoformat()[0:-7].replace(':', '.')
     fname = ('data/loan_cleaned_%s.csv' % file_dt)
-    relevant_features = ['issue_d_ord', 'loan_amnt', 'int_rate', 'term',
-                         'grade', 'emp_length', 'home_ownership',
-                         'annual_inc', 'default']
+    features = ['issue_d_ord', 'loan_amnt', 'int_rate', 'term',
+                'grade', 'emp_length', 'home_ownership', 'annual_inc',
+                'dti', 'open_acc', 'pub_rec', 'purpose', 'revol_bal',
+                'revol_util', 'acc_now_delinq', 'annual_inc']
+    target = ['default']
     df = (pd.read_csv('data/loan.csv', low_memory=False)
           .set_index('id')
           .pipe(status_filter)
@@ -20,12 +23,15 @@ def main(argv):
                   default=lambda x: np.where(x.loan_status=='Fully Paid', 0, 1),
                   issue_d_ord=lambda x: x.issue_d.apply(dt.datetime.toordinal),
                   term=lambda x: x.term.str.strip())
-          .loc[:, relevant_features]
+          [features + target]
           .pipe(make_dummy, 'term')
           .pipe(make_dummy, 'home_ownership')
           .pipe(make_dummy, 'emp_length')
           .pipe(make_dummy, 'grade')
+          .pipe(make_dummy, 'purpose')
           .to_csv(fname))
+
+
     print('Luther Preprocessing Successful Woo Woo!\n'
           'See file located at: %s' % fname)
 
@@ -45,8 +51,11 @@ def emp_length_filter(input_df):
 
 def make_dummy(input_df, column):
     df = input_df.copy()
-    home_dum = (df.loc[:, [column]].pipe(pd.get_dummies))
-    return df.join(home_dum).drop(column, axis=1)
+    dummies = (df.loc[:, [column]]
+               .pipe(pd.get_dummies)
+               .iloc[:, 1:])
+    return df.join(dummies).drop(column, axis=1)
 
 if __name__ == '__main__':
     main(sys.argv)
+
