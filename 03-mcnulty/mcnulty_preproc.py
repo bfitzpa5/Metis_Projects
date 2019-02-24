1540789143c9563dabe4dd1bf6991015dfd82dd5
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-24 10:26:14 -0500

    Added revol_util_filter and updated columns in preproc

--- 03-mcnulty/mcnulty_preproc.py
+++ 03-mcnulty/mcnulty_preproc.py
@@ -5,19 +5,19 @@ import numpy as np
 import datetime as dt
 
 def main(argv):
-
     file_dt = dt.datetime.now().isoformat()[0:-7].replace(':', '.')
     fname = ('data/loan_cleaned_%s.csv' % file_dt)
     features = ['issue_d_ord', 'loan_amnt', 'int_rate', 'term',
                 'grade', 'emp_length', 'home_ownership', 'annual_inc',
                 'dti', 'open_acc', 'pub_rec', 'purpose', 'revol_bal',
-                'revol_util', 'acc_now_delinq', 'annual_inc']
+                'revol_util', 'acc_now_delinq']
     target = ['default']
     df = (pd.read_csv('data/loan.csv', low_memory=False)
           .set_index('id')
           .pipe(status_filter)
           .pipe(remove_annual_inc_nas)
           .pipe(emp_length_filter)
+          .pipe(revol_util_filter)
           .query('home_ownership not in ["NONE", "ANY"]')
           .assign(issue_d=lambda x: x.issue_d.astype('datetime64'),
                   default=lambda x: np.where(x.loan_status=='Fully Paid', 0, 1),
@@ -30,8 +30,6 @@ def main(argv):
           .pipe(make_dummy, 'grade')
           .pipe(make_dummy, 'purpose')
           .to_csv(fname))
-
-
     print('Luther Preprocessing Successful Woo Woo!\n'
           'See file located at: %s' % fname)
 
@@ -47,6 +45,11 @@ def remove_annual_inc_nas(input_df):
 def emp_length_filter(input_df):
     df = input_df.copy()
     na_mask = df.emp_length.isna() | (df.emp_length == 'n/a')
+    return df.loc[~na_mask, ]
+
+def revol_util_filter(input_df):
+    df = input_df.copy()
+    na_mask = df.revol_util.isna()
     return df.loc[~na_mask, ]
 
 def make_dummy(input_df, column):
