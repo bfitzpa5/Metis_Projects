e9ee9174dda025d3bcd1886314284ef09b0c8026
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-02-27 21:51:36 -0500

    Added smote tomeks resampling to mcnulty_rf

--- 03-mcnulty/mcnulty_rf.py
+++ 03-mcnulty/mcnulty_rf.py
@@ -10,7 +10,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.combine import SMOTETomek
 %pylab
+
+def plot_feature_importances(clf, colnames):
+    data = {'features': colnames,
+            'importance': clf.feature_importances_}
+    impt = (pd.DataFrame(data)
+            .sort_values('importance', ascending=False))
+    sns.barplot(x='importance', y='features', order=impt.features, data=data)
 
 pat = re.compile('loan_cleaned_')
 fname = sorted([x for x in os.listdir('data') if pat.match(x)])[-1]
@@ -25,12 +33,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
 
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(X_train, y_train)
-
 mcu.ClassifierDiagnostics(clf, X_test, y_test)
+plot_feature_importances(clf, X_train.columns)
 
-data = {'features': X_train.columns,
-        'importance': clf.feature_importances_}
-impt = (pd.DataFrame(data)
-        .sort_values('importance', ascending=False))
-sns.barplot(x='importance', y='features', order=impt.features, data=data)
-
+smt = SMOTETomek(ratio='auto')
+X_smt, y_smt = smt.fit_sample(X_train, y_train)
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_smt, y_smt)
+mcu.ClassifierDiagnostics(clf, X_test, y_test)
+plot_feature_importances(clf, X_smt.columns)
