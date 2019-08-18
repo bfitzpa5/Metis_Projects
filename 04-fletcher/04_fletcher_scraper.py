7ee48ffffc1c5855ce62e2fac2991b3225f8abd4
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2019-08-18 11:33:51 -0400

    Finish ks_page_scrape in 04_fletcher_scraper.py

--- 04-fletcher/04_fletcher_scraper.py
+++ 04-fletcher/04_fletcher_scraper.py
@@ -7,43 +7,87 @@ Created on Sat Aug 10 23:09:43 2019
 Datapoints
 ---------------
 Name
-Project Description   O
-Location              O
+Project Title         X
+Project Description   X
+Location              X
 About                 O
 Risks                 O
-Goal                  O
+Goal                  X
 Amount Pledged        X
-Days to go            O
-Backers               O
+Days to go            X
+Backers               X
 Category              O
-Subcateogry           O
+SubCategory           X
 """
 
 
 import requests
+import re
 from bs4 import BeautifulSoup
 
 base_url = r'https://www.kickstarter.com/discover'
 
-url = (r'https://www.kickstarter.com/projects/953146955/sleeping-gods'
-       r'?ref=discovery_category')
-response = requests.get(url)  
-
+url = (r'https://www.kickstarter.com/projects/953146955/sleeping-gods')
+response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 def ks_page_scrape(soup):
-amount_pledged = ctoi(soup.find("span", class_="ksr-green-700").text)
-goal = ctoi(soup.find("span", class_="money").text)
-subgenre = soup.find(class_="svg-icon__icon--compass icon-20 fill-soft-black")
-days_to_go = extract_days_to_go(soup)
-print(soup.find("div", class_="description-container").text)
+    project_name = extract_project_name(soup)
+    project_description = extract_project_description(soup)
+    location = extract_location(soup)
+    about = extract_about(soup)
+    risks = extract_risks(soup)
+    amount_pledged = ctoi(soup.find("span", class_="ksr-green-700").text)
+    goal = ctoi(soup.find("span", class_="money").text)
+    backers = extract_backers(soup)
+    days_to_go = extract_days_to_go(soup)
+    subcategory = extract_subcategory(soup)
+    row = {'project_name': project_name,
+           'project_description': project_description,
+           'location': location,
+           'about': about,
+           'risks': risks,
+           'amount_pledged': amount_pledged,
+           'goal': goal,
+           'backers': backers,
+           'days_to_go': days_to_go,
+           'subcategory': subcategory}
+    return row
 
+def extract_project_name(soup):
+    class_pat = re.compile(r'project-name')
+    return soup.find('h2', class_=class_pat).text
 
-def ctoi(s):
-    return int(s.replace('$', '').replace(',', ''))
+def extract_project_description(soup):
+    class_pat = re.compile(r'project-description')
+    return soup.find('p', class_=class_pat).text
+
+def extract_location(soup):
+    class_pat = re.compile(r'map-pin')
+    return soup.find('svg', class_=class_pat).next_sibling.text
+
+def extract_about(soup):
+    class_pat = re.compile(r'full-description')
+    return soup.find('div', class_=class_pat).text
+
+def extract_risks(soup):
+    class_pat = re.compile(r'risks')
+    return soup.find('div', class_=class_pat).text
+
+def extract_subcategory(soup):
+    child_iter = (soup.find(class_="svg-icon__icon--compass icon-20 fill-soft-black")
+                  .next_sibling
+                  .children)
+    return next(child_iter).text
 
 def extract_days_to_go(soup):
     return ctoi(soup.find("div", class_='flex flex-column-lg mb4 mb5-sm')
                 .find("div", class_="ml5 ml0-lg")
                 .find("span")
                 .text)
+
+def extract_backers(soup):
+    return soup.find('span', string='backers').previous
+
+def ctoi(s):
+    return int(s.replace('$', '').replace(',', ''))
