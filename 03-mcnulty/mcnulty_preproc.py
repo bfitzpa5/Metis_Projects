e84fe0016d4562c5c2572a7d8f6856a056cb09fd
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-08-19 21:38:24 -0400

    Tested single feature logistic regression models in mcnulty_modeling.ipynb

--- 03-mcnulty/mcnulty_preproc.py
+++ 03-mcnulty/mcnulty_preproc.py
@@ -7,58 +7,43 @@ import datetime as dt
 def main(argv):
     file_dt = dt.datetime.now().isoformat()[0:-7].replace(':', '.')
     fname = ('data/loan_cleaned_%s.csv' % file_dt)
-    features = ['issue_d_ord', 'loan_amnt', 'int_rate', 'term',
-                'grade', 'emp_length', 'home_ownership', 'annual_inc',
-                'dti', 'open_acc', 'pub_rec', 'purpose', 'revol_bal',
-                'revol_util', 'acc_now_delinq']
-    target = ['default']
     df = (pd.read_csv('data/loan.csv', low_memory=False)
           .set_index('id')
-          .pipe(status_filter)
-          .pipe(remove_annual_inc_nas)
-          .pipe(emp_length_filter)
-          .pipe(revol_util_filter)
-          .query('home_ownership not in ["NONE", "ANY"]')
+          .pipe(loan_status_filter)
+          .query('application_type == "INDIVIDUAL"')
+          .loc[:, features()]
           .assign(issue_d=lambda x: x.issue_d.astype('datetime64'),
                   default=lambda x: np.where(x.loan_status=='Fully Paid', 0, 1),
-                  issue_d_ord=lambda x: x.issue_d.apply(dt.datetime.toordinal),
-                  term=lambda x: x.term.str.strip())
-          [features + target]
+                  term=lambda x: x.term.str.strip(),
+                  emp_length=lambda x: x.emp_length.fillna('Not provided'))
           .pipe(make_dummy, 'term')
           .pipe(make_dummy, 'home_ownership')
           .pipe(make_dummy, 'emp_length')
           .pipe(make_dummy, 'grade')
-          .pipe(make_dummy, 'purpose')
-          .to_csv(fname))
+          .pipe(make_dummy, 'purpose'))
+    df.to_csv(fname)
     print('Luther Preprocessing Successful Woo Woo!\n'
           'See file located at: %s' % fname)
 
-def status_filter(input_df):
-    df = input_df.copy()
-    statuses = ['Fully Paid', 'Charged Off', 'Default']
-    return df.loc[df.loan_status.isin(statuses), ]
+def features():
+    lc_dd = pd.read_excel(r'data/LCDataDictionary.xlsx')
+    qstr = 'Include == 1'
+    cols = [x.strip() for x in lc_dd.query(qstr).LoanStatNew.values.tolist()]
+    cols.remove('id')
+    return cols
 
-def remove_annual_inc_nas(input_df):
-    df = input_df.copy()
-    return df.loc[~df.annual_inc.isna(), ]
 
-def emp_length_filter(input_df):
+def loan_status_filter(input_df):
     df = input_df.copy()
-    na_mask = df.emp_length.isna() | (df.emp_length == 'n/a')
-    return df.loc[~na_mask, ]
-
-def revol_util_filter(input_df):
-    df = input_df.copy()
-    na_mask = df.revol_util.isna()
-    return df.loc[~na_mask, ]
+    loan_status_lst = ['Fully Paid', 'Charged Off', 'Late (31-120 days)', 'Default']
+    mask = df.loan_status.isin(loan_status_lst)
+    return df.loc[mask, :]
 
 def make_dummy(input_df, column):
     df = input_df.copy()
     dummies = (df.loc[:, [column]]
-               .pipe(pd.get_dummies)
-               .iloc[:, 1:])
+               .pipe(pd.get_dummies))
     return df.join(dummies).drop(column, axis=1)
 
 if __name__ == '__main__':
     main(sys.argv)
-
