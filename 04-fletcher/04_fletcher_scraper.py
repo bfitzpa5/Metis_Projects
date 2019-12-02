e07165be9ada9ba4b8b6c7672662ec71912a23a0
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2019-12-01 22:00:16 -0500

    Move from bs4 to selenium in 04_fletcher_scraper.py

--- 04-fletcher/04_fletcher_scraper.py
+++ 04-fletcher/04_fletcher_scraper.py
@@ -4,88 +4,79 @@ Created on Sat Aug 10 23:09:43 2019
 
 @author: Brendan Non-Admin
 
-Datapoints
----------------
-Name
-Project Title         0
-Project Description   0
-Location              0
-About                 0
-Risks                 0
-Goal                  0
-Amount Pledged        0
-Days to go            0
-Backers               0
-Category              0
-SubCategory           0
-Comments Count        0
 """
 
-import re
-from bs4 import BeautifulSoup
-import urllib.request
+import os
+import json
 import pandas as pd
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
 
 def main():
     urls_fname = r'Data/kickstarter_urls.csv'
     urls = (pd.read_csv(urls_fname)
         .assign(url=lambda x: x.url.str.split('?', expand=True)[0])
     )
+    driver = os.path.join(os.path.expanduser('~'), 'Downloads', 'chromedriver.exe')
+    driver = webdriver.Chrome(driver)
+    
     print("Commencing Web Crawl")
     kickstarter_data = list()
-    for index, url in urls.url.iteritems():   
+    for index, url in urls.url[1431:].iteritems():   
         print(url)
-        page_data = ks_scrape(url)
+        page_data = ks_scrape(driver, url)
         if page_data:
             kickstarter_data.append(page_data)
     
-    fname = r'Data/kickstarter_data.csv'
-    (pd.DataFrame(kickstarter_data)
-        .to_csv(fname, index=False)
-    )
+    fname = r'Data/kickstarter_data.json'
+    with open(fname, "w") as f:
+        json.dump(kickstarter_data, f, indent=4, sort_keys=True)
     print('Kickstarter Scraping Successful')
     return 1
 
-def ks_scrape(url):
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-    soup = BeautifulSoup(html, 'html5lib')
+def ks_scrape(driver, url):
+    driver.get(url)
     removal_str = ("This project has been removed from visibility at the "
     "request of the creator. It will remain permanently out of view.")
-    copyright_str = ("is the subject of an intellectual property dispute and is currently unavailable.")
-    if (   soup.find("p", string=re.compile(removal_str)) is not None
-        or soup.find("strong", string=re.compile(copyright_str)) is not None):
+    copyright_str = "is the subject of an intellectual property dispute and is currently unavailable."
+    try:
+        exception_str = ("//*[contains(text(),'{}') or contains(text(),'{}')]"
+                              .format(removal_str, copyright_str))
+        driver.find_element_by_xpath(exception_str)
         return None
+    except NoSuchElementException:
+        pass
     """ Scrape Page """
-    project_name = soup.find('h3', class_="normal").text
-    if soup.find('div', class_="NS_project_profiles__blurb") is not None:
-        project_description = (soup.find('div', class_="NS_project_profiles__blurb")
-            .text)
-    else:
-        project_description = None
-    content = soup.find('section', class_=re.compile('project-content')).find('div').find('div').find('div').text
-    if soup.find("div", class_="spotlight-project-video-archive") is not None:
-        goal_and_pledged = (soup.find("div", class_="spotlight-project-video-archive")
-            .text)
-    else:
-        goal_and_pledged = soup.find("div", role="progressbar").next_sibling.text
-    if soup.find('div', string=re.compile('backers')) is not None:
-        backers = (soup.find('div', string=re.compile('backers'))
-            .parent.text)
-    else:
-        try:
-            backers = soup.find('span', string=re.compile('backes')).previous_sibling.text
-        except:
-            backers = soup.find('span', string=re.compile('backer')).text
-    category_url = (soup.find('a', href=re.compile('/discover/categories'))
-        ['href'])
+    try:
+        project_name = driver.find_element_by_css_selector("div.NS_project_profile__title").text
+    except NoSuchElementException:
+        project_name = driver.find_element_by_css_selector("h2.project-name").text
+    try:
+        project_description = driver.find_element_by_css_selector("div.NS_project_profiles__blurb").text
+    except NoSuchElementException:
+        project_description = driver.find_element_by_css_selector("p.project-description").text
+    try:
+        story = driver.find_element_by_class_name("rte__content").text
+    except NoSuchElementException:
+        story = None
+    try:
+        risks = driver.find_element_by_css_selector("div#risksAndChallenges").text
+    except NoSuchElementException:
+        risks = None
+    try:
+        goal_and_pledged_backers = driver.find_element_by_css_selector("div.spotlight-project-video-archive").text
+    except NoSuchElementException:
+        goal_and_pledged_backers = driver.find_element_by_xpath('//*[@id="react-project-header"]/div/div/div[3]/div').text
+    category_url = (driver.find_element_by_css_selector("a[href*='/discover/categories']")
+        .get_attribute("href"))
     """ Unload Data """
     row = {'url': url,
            'project_name': project_name,
            'project_description': project_description,
-           'content': content,
-           'goal_and_pledged': goal_and_pledged,
-           'backers': backers,
+           'story': story,
+           'risks': risks,
+           'goal_and_pledged_backers': goal_and_pledged_backers,
            'category_url': category_url,}
     return row
 
