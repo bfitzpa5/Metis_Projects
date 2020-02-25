206c026d8926da7b7114880e7c65efca24fd9a11
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-25 08:41:12 -0500

    Create 05-kojak_twitter_api.py

---
+++ 05-kojak/05-kojak_twitter_api.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 07:53:02 2020
+
+@author: Brendan Non-Admin
+"""
+
+import pandas as pd
+import requests
+import base64
+import json
+from twitter_config import tc
+
+key_secret = '{}:{}'.format(tc['api_key'], tc['api_secret_key']).encode('ascii')
+
+b64_encoded_key = base64.b64encode(key_secret)
+
+b64_encoded_key = b64_encoded_key.decode('ascii')
+
+base_url = 'https://api.twitter.com/'
+
+auth_url = '{}oauth2/token'.format(base_url)
+
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+access_token = auth_resp.json()['access_token']
+
+search_headers = {
+    'Authorization': 'Bearer {}'.format(access_token)    
+}
+search_params = {
+    'q': 'Potter',
+    'result_type': 'recent',
+    'count': 10
+}
+search_url = '{}1.1/search/tweets.json'.format(base_url)
+search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+
+data = json.loads(search_resp.content)
+
+tweet_texts = pd.DataFrame(data['statuses']).text
