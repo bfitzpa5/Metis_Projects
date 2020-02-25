70d715c0164acbe3807a5db7fb8673f3ac3e0bba
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-25 16:56:58 -0500

    Make 05-kojak_twitter_api.py an executable

--- 05-kojak/05-kojak_twitter_api.py
+++ 05-kojak/05-kojak_twitter_api.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 25 07:53:02 2020
-
 @author: Brendan Non-Admin
 """
 
+import os
 import pandas as pd
 import requests
 import base64
 import json
 from twitter_config import tc
 
+base_url = 'https://api.twitter.com/'
+
 def authenticate():
     key_secret = '{}:{}'.format(tc['api_key'], tc['api_secret_key']).encode('ascii')
     b64_encoded_key = base64.b64encode(key_secret)
     b64_encoded_key = b64_encoded_key.decode('ascii')
-    base_url = 'https://api.twitter.com/'
     auth_url = '{}oauth2/token'.format(base_url)
     auth_headers = {
         'Authorization': 'Basic {}'.format(b64_encoded_key),
@@ -28,19 +29,42 @@ def authenticate():
     access_token = auth_resp.json()['access_token']
     return access_token
 
-access_token = authenticate()
+def tweet_df(term, access_token):
+    search_headers = {
+        'Authorization': 'Bearer {}'.format(access_token)    
+    }
+    search_params = {
+        'q': term,
+        'lang': 'en',
+        #'result_type': 'popular',
+    }
+    search_url = '{}1.1/search/tweets.json'.format(base_url)
+    search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+    
+    data = json.loads(search_resp.content)
+    df = pd.DataFrame(data['statuses'])
+    
+    return df
 
-search_headers = {
-    'Authorization': 'Bearer {}'.format(access_token)    
-}
-search_params = {
-    'q': 'Potter',
-    'result_type': 'recent',
-    'count': 10
-}
-search_url = '{}1.1/search/tweets.json'.format(base_url)
-search_resp = requests.get(search_url, headers=search_headers, params=search_params)
 
-data = json.loads(search_resp.content)
-
-tweet_texts = pd.DataFrame(data['statuses']).text
+def main():
+    access_token = authenticate()
+    
+    houses = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw']
+    frames = list()
+    for house in houses:
+        df = tweet_df(house, access_token)
+        df.loc[:, 'house'] = house
+        frames.append(df)
+        
+    df = pd.concat(frames, sort=False)
+    df = df.set_index(df.house + '_' + df.id_str)
+    
+    fout = os.path.join(
+        'data',
+        'house_tweets.json'   
+    )
+    df.to_json(fout, orient='records')
+    
+if __name__ == '__main__':
+    main()
