b5743f3f1e9c216e86ed9cceb38aa10dded09321
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-25 11:55:21 -0500

    Update 05-kojak_twitter_api to make authenticate function

--- 05-kojak/05-kojak_twitter_api.py
+++ 05-kojak/05-kojak_twitter_api.py
@@ -11,26 +11,24 @@ import base64
 import json
 from twitter_config import tc
 
-key_secret = '{}:{}'.format(tc['api_key'], tc['api_secret_key']).encode('ascii')
+def authenticate():
+    key_secret = '{}:{}'.format(tc['api_key'], tc['api_secret_key']).encode('ascii')
+    b64_encoded_key = base64.b64encode(key_secret)
+    b64_encoded_key = b64_encoded_key.decode('ascii')
+    base_url = 'https://api.twitter.com/'
+    auth_url = '{}oauth2/token'.format(base_url)
+    auth_headers = {
+        'Authorization': 'Basic {}'.format(b64_encoded_key),
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    auth_data = {
+        'grant_type': 'client_credentials'
+    }
+    auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+    access_token = auth_resp.json()['access_token']
+    return access_token
 
-b64_encoded_key = base64.b64encode(key_secret)
-
-b64_encoded_key = b64_encoded_key.decode('ascii')
-
-base_url = 'https://api.twitter.com/'
-
-auth_url = '{}oauth2/token'.format(base_url)
-
-auth_headers = {
-    'Authorization': 'Basic {}'.format(b64_encoded_key),
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-}
-auth_data = {
-    'grant_type': 'client_credentials'
-}
-auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-
-access_token = auth_resp.json()['access_token']
+access_token = authenticate()
 
 search_headers = {
     'Authorization': 'Bearer {}'.format(access_token)    
