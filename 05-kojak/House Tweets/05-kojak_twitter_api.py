fecf1c411f77e415d8149c84882ea0e6cc7d1641
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-07-17 10:48:02 -0400

    Update kojak twitter pull

--- 05-kojak/House Tweets/05-kojak_twitter_api.py
+++ 05-kojak/House Tweets/05-kojak_twitter_api.py
@@ -30,22 +30,26 @@ def authenticate():
     access_token = auth_resp.json()['access_token']
     return access_token
 
-def tweet_df(term, access_token):
-    search_headers = {
+def tweet_df(term, access_token, next_id=None):
+    request_headers = {
         'Authorization': 'Bearer {}'.format(access_token)    
     }
-    search_params = {
-        'q': term,
-        'lang': 'en',
-        #'result_type': 'popular',
+    request_params = {
+        'query': house,
+        #'count': 200,
     }
-    search_url = '{}1.1/search/tweets.json'.format(_base_url)
-    search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+    if next_id:
+        request_params['next'] = next_id
+
+    url = 'https://api.twitter.com/1.1/tweets/search/fullarchive/nlpanalysis.json'
+    response = requests.get(url, headers=request_headers, params=request_params)
+
+    json_data = response.json()
+    df = pd.DataFrame(json_data['results'])
     
-    data = json.loads(search_resp.content)
-    df = pd.DataFrame(data['statuses'])
+    response_next_id = json_data['next']
     
-    return df
+    return df, response_next_id 
 
 
 def main():
@@ -54,12 +58,17 @@ def main():
     houses = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw']
     frames = list()
     for house in houses:
-        df = tweet_df(house, access_token)
-        df.loc[:, 'house'] = house
-        frames.append(df)
-        
+        next_id = None
+        while True:
+            df, next_id = tweet_df(house, access_token, next_id)
+            df.loc[:, 'house'] = house
+            frames.append(df)
+            if next_id is None:
+                break
+    
     df = pd.concat(frames, sort=False)
     df = df.set_index(df.house + '_' + df.id_str)
+    df.shape
     ts = dt.datetime.now().strftime('%y%m%d')
     fout = os.path.join(
         'data',
