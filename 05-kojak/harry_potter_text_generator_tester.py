f1f28aea1c81b29f00dd34ebc0ac98b21223ec9a
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-19 17:58:45 -0400

    Update harry potter text generator with output func

--- 05-kojak/harry_potter_text_generator_tester.py
+++ 05-kojak/harry_potter_text_generator_tester.py
@@ -5,47 +5,67 @@ Created on Wed Aug  5 14:50:53 2020
 @author: Brendan Non-Admin
 """
 
+import os
 import spacy
 import kojak_utils as utils
-from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer # required for the pickle load
 from keras.models import load_model
 from pickle import load
 from numpy import random
 
-with open('hp_textgen_tokenizer.pkl', 'rb') as f:
-    tokenizer = load(f)
 
-model = load_model('hp_textgen.h5')
-
-book_text = utils.read_book_texts()
-nlp = spacy.load('en',disable=['parser', 'tagger','ner'])
-tokens = utils.separate_punc(nlp(book_text))
-
-
-f = open('hp_tokenizer_model_tests.txt', 'w')
-
-seq_len = 30
-num_gen_words = 100
-
-random.seed(11)
-randos = random.randint(0, len(tokens)-seq_len, 10)
-for rando in randos:
-    seed_text = ' '.join(tokens[rando: rando+seq_len])
+def main():
+    modelspath = os.path.join('Data', 'Models')
+    filepath = os.path.join(modelspath, 'hp_textgen_tokenizer.pkl')
+    with open(filepath, 'rb') as f:
+        tokenizer = load(f)
     
-    f.write(seed_text + '\n')
-    f.write('='*160 + '\n')
+    filepath = os.path.join(modelspath, 'hp_textgen.h5')
+    model = load_model(filepath)
     
-    args = [model, tokenizer, seq_len, seed_text, num_gen_words]
-    generated_text = utils.generate_text(*args)
+    book_text = utils.read_book_texts()
+    nlp = spacy.load('en',disable=['parser', 'tagger','ner'])
+    tokens = utils.separate_punc(nlp(book_text))
+    
+    
+    filepath = os.path.join(modelspath, 'hp_tokenizer_model_tests.txt')
+    f = open(filepath, 'w')
+    
+    seq_len = 30
+    num_gen_words = 100
+    
+    random.seed(11)
+    randos = random.randint(0, len(tokens)-seq_len, 10)
+    
+    for rando in randos:
+        seed_text = ' '.join(tokens[rando: rando+seq_len])
+        
+        args = [model, tokenizer, seq_len, seed_text, num_gen_words]
+        generated_text = utils.generate_text(*args)
+        
+        output_generated_text(seed_text, generated_text, seq_len, f.write)
+        
+        f.write('\n\n\n')
+    
+    f.close()
+
+def output_generated_text(seed_text, generated_text, seq_len, output_func):
+    if output_func == print:
+        kwargs = dict(end="")
+    else:
+        kwargs = dict()
+    
+    output_func(seed_text + '\n', **kwargs)
+    output_func('='*160 + '\n', **kwargs)
     
     generated_text_tokens = generated_text.split(' ')
     
     i = seq_len
     while i < len(generated_text_tokens):
-        f.write(' '.join(generated_text_tokens[i-seq_len:i]))
-        f.write('\n')
+        output_func(' '.join(generated_text_tokens[i-seq_len:i]), **kwargs)
+        output_func('\n', **kwargs)
         i = i + seq_len
-    
-    f.write('\n\n\n')
 
-f.close()
+
+if __name__ == '__main__':
+    main()
